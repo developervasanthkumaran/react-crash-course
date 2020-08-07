df1182cc267f7a4faf1109a333028f98bde7fc9f
developervasanthkumaran <developer.vasanthkumaran@gmail.com>
2020-08-07 21:24:16 +0530

    commiting local changes Please enter the commit message for your changes. Lines starting

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Showing simple component demo</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
