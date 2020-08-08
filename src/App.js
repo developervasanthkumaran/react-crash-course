cb4675d6694c4dbcf82285f3120ef78028631b4d
developervasanthkumaran <developer.vasanthkumaran@gmail.com>
2020-08-08 17:07:27 +0530

    used react-spring for transitions and react-router for routing pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React from 'react'
 import HelloWorld from './components/HelloWorld'
 import Counter from './components/Counter'
+import Header from './components/Header'
+import Footer from "./components/Footer"; 
+import {BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Views/Home' 
+import About from './Views/About'
+
 function App() {
   return (
-    <div>
-    {/* <HelloWorld name="Hussey"/> */}
-   <Counter/>
-    </div>
+    <Router>
+    <Header/>
+    <Footer/>
+   
+   
+    <Switch>
+        <Route exact path="/">
+         <Home/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+      </Switch>
+  
+    </Router>
+
+
+
   );
 }
 
